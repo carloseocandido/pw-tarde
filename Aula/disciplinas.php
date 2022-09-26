fe40a678e639946fad0eed631c211cf3123a0c15
carloseocandido <carloseocandido@gmail.com>
2022-09-26 16:04:38 -0300

    consumir API

--- Aula/disciplinas.php
+++ Aula/disciplinas.php
@@ -1,52 +1,80 @@
 <?php include "./cabecalho.php"; ?>
 
-    <h1>Disciplinas</h1>
+<h1 class="text-center mt-3">Disciplinas</h1>
 
-    <?php
-        $dados = file_get_contents("https://reserva.fatectq.edu.br/api/disciplinas"); // Pega o conteudo do site (no caso json)
-        $dados = json_decode($dados, true); // Decodifica o json para virar array
-        // echo "<pre>";
-        // print_r($dados);
-        // echo "</pre>";
-    ?>
+<?php
 
-    <div class="row">
-        <div class="col-10 offset-1">
-            <table class="table table-dark table-striped text-center">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Apelido</th>
-                        <th>Sigla</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
+if (isset($_GET["pesquisa"]) && !empty($_GET["pesquisa"])) {
+    $dados = file_get_contents("https://reserva.fatectq.edu.br/api/disciplinas/busca/".urlencode($_GET["pesquisa"])); // Pega o conteudo do site (no caso json)
+    $dados = json_decode($dados, true); // Decodifica o json para virar array
+} else {
+    $dados = file_get_contents("https://reserva.fatectq.edu.br/api/disciplinas"); // Pega o conteudo do site (no caso json)
+    $dados = json_decode($dados, true); // Decodifica o json para virar array
+}
+// echo "<pre>";
+// print_r($dados);
+// echo "</pre>";
+?>
 
-                    <?php
-                        for($i = 0; $i < count($dados); $i++){
-                    ?>
-
-                        <tr>
-                            <td><?php echo $dados[$i]["disciplinaId"]; ?></td>
-                            <td><?php echo $dados[$i]["nome"]; ?></td>
-                            <td><?php echo $dados[$i]["disciplinaId"]; ?></td>
-                            <td><?php echo $dados[$i]["sigla"]; ?></td>
-                            <td>
-                                <a href="./editar_disciplina.php?id=<?php echo $dados[$i]["disciplinaId"];?>" class="btn btn-warning">
-                                    Editar
-                                </a>
-                            </td>
-                        </tr>
-
-                    <?php
-                        }
-                    ?>
-
-                </tbody>
-            </table>
+<div class="row">
+    <div class="col-10 offset-1">
+        <div class="card mt-3 card text-white bg-dark mb-3">
+            <div class="card-header">Pesquisar Disciplinas</div>
+            <div class="card-body">
+                <form action="./disciplinas.php" method="get">
+                    <div class="row">
+                        <!-- Abertura da row -->
+                        <div class="col-2">
+                            <a href="" class="btn btn-success">
+                                Nova Disciplina
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <input type="text" name="pesquisa" class="form-control" placeholder="Digite aqui">
+                        </div>
+                        <div class="col-2">
+                            <button type="submit" class="btn btn-primary">Pesquisar</button>
+                        </div>
+                    </div> <!-- Fechamento row -->
+                </form>
+            </div>
         </div>
+
+        <table class="table table-dark table-striped text-center table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Apelido</th>
+                    <th>Sigla</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                for ($i = 0; $i < count($dados); $i++) {
+                ?>
+
+                    <tr>
+                        <td><?php echo $dados[$i]["disciplinaId"]; ?></td>
+                        <td><?php echo $dados[$i]["nome"]; ?></td>
+                        <td><?php echo $dados[$i]["disciplinaId"]; ?></td>
+                        <td><?php echo $dados[$i]["sigla"]; ?></td>
+                        <td>
+                            <a href="./editar_disciplina.php?id=<?php echo $dados[$i]["disciplinaId"]; ?>" class="btn btn-warning">
+                                Editar
+                            </a>
+                        </td>
+                    </tr>
+
+                <?php
+                }
+                ?>
+
+            </tbody>
+        </table>
     </div>
+</div>
 
 <?php include "./rodape.php"; ?>
--- Aula/rodape.php
+++ Aula/rodape.php
@@ -1,17 +1,37 @@
+</div>
 
-    </div>
-        <p>Copyright&copy;Carlos Candido
+
+<script src="./script/bootstrap.bundle.min.js"></script>
+
+</body>
+
+<footer>
+    <p>Copyright&copy;Carlos Candido
         <script>
             var today = new Date();
             var year = today.getFullYear()
             var week = today.getDay()
+            var month = today.getMonth()
             document.write(year)
-        </script></p>
+            if(week.value == "0"){  
+                document.write(` Domingo`)
+            } else if (week == "1") {
+                document.write(" Segunda-Feira")
+            } else if (week == "2") {
+                document.write(" Terça-Feira")
+            } else if (week == "3") {
+                document.write(" Quarta-Feira")
+            } else if (week == "4") {
+                document.write(" Quinta-Feira")
+            } else if (week == "5") {
+                document.write(" Sexta-Feira")
+            } else {
+                document.write(" Sabado")
+            }
+        </script>
+    </p>
+</footer>
 
-    
-    <script src="./script/bootstrap.bundle.min.js"></script>
-
-</body>
 </html>
 
 <?php
